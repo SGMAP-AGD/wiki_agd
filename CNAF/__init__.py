ed18b013c0f33356ce040402c1d76f95c0da0e12
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-09-22 19:21:05 +0200

    __init__

---
+++ CNAF/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:21:57 2015
+
+@author: Alexis Eidelman
+"""
+
---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:22:09 2015
+
+@author: Alexis Eidelman
+"""
+
