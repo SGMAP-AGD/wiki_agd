bc913793ba0cf4c0ec8be046447f178bb34562ba
paul <paul.bouchequet@gmail.com>
2015-09-24 10:20:40 +0200

    dict tables_variables

--- CNAF/read_excel.py
+++ CNAF/read_excel.py
@@ -8,7 +8,7 @@ Created on Mon Sep 21 22:39:43 2015
 
 import pandas as pd
 
-path_doc = 'D:/data/code_cnaf/tables_etalab.xls'
+path_doc = '/home/paul/wiki_agd/CNAF/shortcut/tables_etalab.xls'
 
 list_sheets = ['AGEN', 'FIL', 'GCA',
                'WEB', 'SDSUI']
@@ -20,3 +20,23 @@ for sheet in list_sheets:
     variables_a_garder[sheet] = tab.Nom.tolist()
 
 variables_a_garder['FRE_CODE_INSEE'] = variables_a_garder['AGEN']
+
+
+"""
+Le dictionnaire tables_variables contient en clé toutes les variables présentes dans le dictionnaire variables_a_garder et en valeur une liste de toutes les tables dans lesquelles la variable est présente.
+
+@author boupetch
+"""
+tables_variables = dict()
+
+for table in variables_a_garder:
+    for variable in variables_a_garder[table]:
+        if variable in tables_variables:
+            tables_variables[variable].append(table)  
+        else:
+            tables_variables[variable] = [table]
+        
+
+
+
+
